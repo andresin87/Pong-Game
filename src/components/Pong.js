baff7b9dc1d4f2c13a3ccad04ddeea8ce845615d
Andrés Lucas <alucas@redpoints.com>
2017-11-08 19:42:45 +0100

    ADD Players pannels.

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -33,12 +33,27 @@ class Rect {
   get bottom() {
     return this.pos.y + this.size.y / 2;
   }
+  get bounds() {
+    return {
+      top: this.top(),
+      bottom: this.bottom(),
+      right: this.right(),
+      left: this.left(),
+    };
+  }
 }
 
 class Ball extends Rect {
   constructor() {
     super(10, 10);
     this.vel = new Vector;
+  }
+}
+
+class Player extends Rect {
+  constructor() {
+    super(20, 100);
+    this.score = 0;
   }
 }
 
@@ -49,8 +64,18 @@ class Game {
 
     // Initialise the ball
     this.ball = new Ball;
-    ball.vel.x = 100;
-    ball.vel.y = 100;
+    this.ball.vel.x = 100;
+    this.ball.vel.y = 100;
+
+    this.players = [
+      new Player,
+      new Player,
+    ];
+    this.players[0].pos.x = 40;
+    this.players[1].pos.x = this.canvas.width - 40;
+    this.players.forEach(player => {
+      player.pos.y = this.canvas.height / 2;
+    });
 
     let lastTime;
     const callback = (millis) => {
@@ -68,13 +93,19 @@ class Game {
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.drawRect(this.ball);
+    this.players.forEach(player => this.drawRect(player));
   }
   drawRect(rect) {
     // Update the ball
     this.context.fillStyle = '#ffffff';
-    this.context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
+    this.context.fillRect(
+      rect.left,
+      rect.top,
+      rect.size.x,
+      rect.size.y,
+    );
   }
-  update(dt) => {
+  update(dt) {
     this.ball.pos.x += this.ball.vel.x * dt;
     this.ball.pos.y += this.ball.vel.y * dt;
 
@@ -94,7 +125,7 @@ class Pong extends React.Component {
     return false;
   }
   componentDidMount() {
-    const Game = new Game(this.canvas);
+    this.Game = new Game(this.canvas);
   }
   render() {
     const { width, height } = this.props
