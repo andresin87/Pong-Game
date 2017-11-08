2601adf95a9500c16e6a0737bb6b34a47f06f09d
Andrés Lucas <alucas@redpoints.com>
2017-11-08 15:11:21 +0100

    ADD Bounding margins.

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -21,6 +21,18 @@ class Rect {
     this.pos = new Vector;
     this.size = new Vector(w, h);
   }
+  get left() {
+    return this.pos.x - this.size.x / 2;
+  }
+  get right() {
+    return this.pos.x + this.size.x / 2;
+  }
+  get top() {
+    return this.pos.y - this.size.y / 2;
+  }
+  get bottom() {
+    return this.pos.y + this.size.y / 2;
+  }
 }
 
 class Ball extends Rect {
@@ -55,6 +67,13 @@ class Pong extends React.Component {
     const update = (dt) => {
       ball.pos.x += ball.vel.x * dt;
       ball.pos.y += ball.vel.y * dt;
+
+      if (ball.left < 0 || ball.right > canvas.width) {
+        ball.vel.x = -ball.vel.x;
+      }
+      if (ball.top < 0 || ball.bottom > canvas.height) {
+        ball.vel.y = -ball.vel.y;
+      }
 
       // Add Background
       this.context.fillStyle = '#000000';
