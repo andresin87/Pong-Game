7815e6b7d5675b46351fecbd9b57d4139a603a65
Andrés Lucas <alucas@redpoints.com>
2017-11-08 21:59:03 +0100

    RM yeoman default template rendering

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,8 +11,6 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
         <div className="pong-wrapper">
           <Pong width={800} height={600}/>
         </div>
