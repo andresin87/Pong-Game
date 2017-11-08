2eb84674aba84e461579afd645ec03e67e464567
Andrés Lucas <alucas@redpoints.com>
2017-11-08 20:14:49 +0100

    MOD start with random initial values

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -123,8 +123,8 @@ class Game {
   }
   start() {
     if (this.ball.vel.x === 0 && this.ball.vel.y === 0) {
-      this.ball.vel.x = 300;
-      this.ball.vel.y = 300;
+      this.ball.vel.x = 300 * (Math.random() > .5 ? 1 : -1);
+      this.ball.vel.y = 300 * (Math.random() > .5 ? 2 : 1) * (Math.random() > .5 ? -1 : 1);
     }
   }
   update(dt) {
