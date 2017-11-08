a21e67fc9e7475ef45854582f97808be2b374c33
Andrés Lucas <alucas@redpoints.com>
2017-11-08 13:53:57 +0100

    ADD Pong module, Vector, Rect, Ball classes and first ball movement cycles.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,17 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
+import Pong from './Pong';
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className="pong-wrapper">
+          <Pong width={800} height={600}/>
+        </div>
       </div>
     );
   }
---
+++ src/components/Pong.js
@@ -0,0 +1,94 @@
+/**
+ * Company: Redpoints.
+ * Website: www.redpoints.com
+ * Date: 08/11/2017
+ * Project: Pong-Game
+ * Description: The description here
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Rect {
+  constructor(w, h) {
+    this.pos = new Vector;
+    this.size = new Vector(w, h);
+  }
+}
+
+class Ball extends Rect {
+  constructor() {
+    super(10, 10);
+    this.vel = new Vector;
+  }
+}
+
+class Pong extends React.Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  componentDidMount() {
+    const canvas = this.canvas;
+    this.context = canvas.getContext('2d');
+
+    const ball = new Ball;
+    ball.vel.x = 100;
+    ball.vel.y = 100;
+
+    let lastTime;
+
+    const callback = (millis) => {
+      if (lastTime) {
+        update((millis - lastTime) / 1000);
+      }
+      lastTime = millis;
+      requestAnimationFrame(callback);
+    };
+
+    const update = (dt) => {
+      ball.pos.x += ball.vel.x * dt;
+      ball.pos.y += ball.vel.y * dt;
+
+      // Add Background
+      this.context.fillStyle = '#000000';
+      this.context.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Add the ball
+      this.context.fillStyle = '#ffffff';
+      this.context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+    }
+
+    callback();
+
+  }
+  render() {
+    const { width, height } = this.props
+    return (
+      <canvas
+        id="pong"
+        ref={(c) => { this.canvas = c; }}
+        width={width}
+        height={height}
+      />
+    );
+  }
+}
+
+Pong.PropTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+Pong.defaultProps = {
+  width: 600,
+  height: 600,
+};
+
+export default Pong;
